723abab53eb54fea8aa222487a857674144cd573
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-05-07 01:18:00 +0200

    Add createTweet script

--- js/script.js
+++ js/script.js
@@ -5,8 +5,30 @@ function getQuote() {
   fetch(quoteUrl, {
       cache: 'no-store'
     })
-    .then(function(resp)) {
+    .then(function(resp) {
       return resp.json();
     })
 .then(createTweet);
+}
+
+function createTweet(input) {
+  var data = input[0];
+
+  var dataElement = document.createElement('div');
+  dataElement.innerHTML = data.content;
+  var quoteText = dataElement.innerText.trim();
+  var quoteAuthor = data.title;
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+  if (!quoteAuthor.length) {
+    quoteAuthor = "Unknown author";
+  }
+  if (tweetText.length > 140) {
+    getQuote();
+  } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    document.querySelector('.quote').innerText = quoteText;
+    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+    document.querySelector('.tweet').setAttribute('href', tweet);
+  }
 }
